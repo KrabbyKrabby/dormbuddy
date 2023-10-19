4848b5047c2fb0c9c26a6350f85bd4bdd7a13667
Ittehad <chowdhuryittehad@gmail.com>
2023-10-19 23:27:50 +0600

    Completed Login Page

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -4,11 +4,16 @@ export default function LoginCard(){
     return(
         <div className="loginCard">
             <div className="upperCard">
-            
+                <h1>Login</h1>
+                <input type="text" placeholder="Email" className="emailInput"></input>
+                <input type="text" placeholder="Password" className="passwordInput"></input>
+                <button className="forgotPassword">Forgot Password?</button>
+                <button className="loginButton">Login</button>
             </div>
-
+                
             <div className="lowerCard">
-
+                <h1 className="notYetRegistered">Not yet registered?</h1>
+                <button className="register">Register</button>
             </div>
         </div>
     );
