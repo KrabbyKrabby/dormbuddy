9281911813e9cbfe8df3080c05b7be1138aaf2be
Ittehad <chowdhuryittehad@gmail.com>
2023-10-20 19:51:23 +0600

    added Drom Card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LoginCard from './components/LoginCard';
+import DormCard from './components/DormCard';
 import './index.css';
 
 function App() {
   return (
     <main className="main">
       <Navbar />
-      <LoginCard />
+      <DormCard />
       <Footer />
     </main>
   );
---
+++ src/components/DormCard.js
@@ -0,0 +1,32 @@
+import WindowLogo from '../images/window.png'
+import KitchenLogo from '../images/kitchen.png'
+import LocationLogo from '../images/Location.png'
+import ToiletLogo from '../images/toilet.png'
+import './CSS/DormCard.css'
+
+export default function(){
+    return(
+        <div className="dormCard">
+            <h1 className="dormTitle">Newly built 4 Bedroom Apartment</h1>
+            <div className="innerCard">
+                <img className='dormImage' src="https://parkandrefer.com/wp-content/uploads/2015/03/nice-house.png"></img>
+                <div className="dormInfo">
+                    <div className="dormCity">
+                        <img src={LocationLogo} alt="Location Logo" className="locationLogo"></img>
+                        <h1 className="dormCityName">Dhaka</h1>
+                    </div>
+                    <h1 className="dormRent">$5000</h1>
+                    <h1 className="dormAddress">nazimuddin road</h1>
+                    <div className="dormAmenities">
+                        <img src={WindowLogo} alt="Window Logo" className="windowLogo"></img>
+                        <h1 className='windowNo'>4</h1>
+                        <img src={KitchenLogo} alt="Kitchen Logo" className="kitchenLogo"></img>
+                        <h1 className='kitchenAvailable'>Available</h1>
+                        <img src={ToiletLogo} alt="Toilet Logo" className="toiletLogo"></img>
+                        <h1 className='toiletAvailable'>Not Available</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
