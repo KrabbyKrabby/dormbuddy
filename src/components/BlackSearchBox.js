9c276050ca3b03ac2abebad24081dd46f819593c
Ittehad <chowdhuryittehad@gmail.com>
2023-10-27 02:01:46 +0600

    added useState to Homepage

--- src/components/BlackSearchBox.js
+++ src/components/BlackSearchBox.js
@@ -1,22 +1,39 @@
 import './CSS/BlackSearchBox.css'
+import React from 'react';
 export default function BlackSearchBox() {
+
+    const [formData, setFormData] = React.useState({ location: "", rent: "", size: "", toilet: true, kitchen: true })
+
+    function handleChange(e) {
+        const { name, value, type, checked } = e.target
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+    function handleSubmit(){
+        console.log(formData)
+    }
+
     return(
-        <div className='blackSearchBox'>
+        <form className='blackSearchBox' onSubmit={handleSubmit}>
             <h1 className='searchTitle'>Find The Apartment Of Your Taste</h1>
-            <input type='text' placeholder='Location?' className='locationHomepage'/>
+            <input type='text' placeholder='Location?' className='locationHomepage' onChange={handleChange} name="location" value={formData.location}/>
             <div className='searchContainer'>
-                <input type="text" className="rentHomepage" placeholder="Rent?"></input>
-                <input type="text" className="sizeHomepage" placeholder="Size?"></input>
+                <input type="text" className="rentHomepage" placeholder="Rent?" onChange={handleChange} name="rent" value={formData.rent}></input>
+                <input type="text" className="sizeHomepage" placeholder="Size?" onChange={handleChange} name="size" value={formData.size}></input>
             </div>
 
             <div className='radioButtonContainer'>
-                 <input type = "checkbox" className="toiletHomepage" id = "toilet" name = "toilet"  />
+                <input type = "checkbox" className="toiletHomepage" id = "toilet" name = "toilet" onChange = {handleChange} checked = {formData.toilet}  />
                 <label htmlFor="toilet">Attached Toilet?</label>
             
-                <input type = "checkbox" className="kitchenHomepage" id = "kitchen" name = "kitchen"  />
+                <input type = "checkbox" className="kitchenHomepage" id = "kitchen" name = "kitchen" onChange = {handleChange} checked = {formData.kitchen}  />
                 <label htmlFor="kitchen">Kitchen?</label>
             </div>
              <button className="filterButtonHomepage">Search</button>
-        </div>
+        </form>
     );
 }
