354a98db57bf9ea6f7b8202eb66806c0a37a5ff4
Ittehad <chowdhuryittehad@gmail.com>
2023-11-11 20:53:15 +0600

    Post method test

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,7 +6,8 @@ import './CSS/LoginPage.css';
 import axios from 'axios';
 import React from 'react';
 
-const baseURL = "https://api.imgflip.com/get_memes"
+const getURL = "https://tourismpracticespring-production.up.railway.app/api/v1/tourism"
+const postURL = "https://tourismpracticespring-production.up.railway.app/api/v1/tourism/add"
 
 export default function LoginPage(props) {
 
@@ -14,19 +15,33 @@ export default function LoginPage(props) {
     const [post, setPost] = React.useState(null);
 
     React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
-                setPost(response.data);
-            })
+        axios.get(getURL).then((response) => {
+            setPost(response.data);
+        })
             .catch((err) => {
                 console.log(err);
             })
     });
 
+    function createPost() {
+        axios.post(postURL, {
+            spotName: "go right fn",
+            openingDate: "2021-01-01",
+            closingDate: "2021-12-31",
+            description: "no no ",
+            location: "France"
+        })
+            .then((response) => {
+                setPost(response.data);
+            });
+    }
+
     return (
         <main className="main">
-            <Navbar {...props}/>
+            <Navbar {...props} />
             <h1>{JSON.stringify(post)}</h1>
-            <LoginCard {...props}/>
+            <button onClick={createPost}>Submit</button>
+            <LoginCard {...props} />
             <Footer />
         </main>
     );
