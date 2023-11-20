5652a06e31ad351b649e3280040c41246eb8257c
Ittehad <chowdhuryittehad@gmail.com>
2023-11-20 08:27:49 +0600

    Added About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,26 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ProfileCard from "../components/ProfileCard";
+import Profiles from "../ProfileData.js"
+import "./CSS/AboutPage.css";
 export default function(){
+
+    const ProfileCards = Profiles.map((profile) => {
+        return (
+            <ProfileCard 
+                item = {profile}
+            />
+        );
+    });
+
     return(
-        <h1>Hello</h1>
+        <main>
+            <Navbar/>
+            <div className="profileContainer">
+                {ProfileCards}
+            </div>
+            <Footer/>
+        </main>
+        
     );
 }
